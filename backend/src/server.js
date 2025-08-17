0a3065104c1c5242362e03f94ca8bb2ca72e1d7f
Vivek Gangdhar <vivekgangdhar11@gmail.com>
2025-08-17 19:07:20 +0530

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,26 +1,39 @@
 import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
-import {connectDB} from "./config/db.js";
+import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 connectDB();
 
-app.use(cors({
-  origin:"http://localhost:5173",
-}))
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 
 app.use(express.json());
 app.use(rateLimiter);
 
-
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(5001, () => {
   console.log("Server is running on port 5001");
