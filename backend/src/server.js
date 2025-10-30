5e97f24acfe2346d593567581f47b1cad996930b
Vivek Gangdhar <vivekgangdhar11@gmail.com>
2025-10-30 23:33:34 +0530

    minor change

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,40 +3,25 @@ import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import cors from "cors";
-import path from "path";
 
 dotenv.config();
-
 const app = express();
 const PORT = process.env.PORT || 5001;
-const __dirname = path.resolve();
 
 // Connect to MongoDB
 connectDB();
 
 // Middleware
-if (process.env.NODE_ENV !== "production") {
-  app.use(
-    cors({
-      origin: "http://localhost:5173",
-    })
-  );
-}
-
+app.use(cors()); // Allow all origins since you’ll manually connect from frontend
 app.use(express.json());
 
 // Routes
 app.use("/api/notes", notesRoutes);
 
-// Serve frontend (React build) in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
-  });
-}
-
+// Default route
+app.get("/", (req, res) => {
+  res.send("✅ Backend is running successfully!");
+});
 
 // Start server
 app.listen(PORT, () => {
