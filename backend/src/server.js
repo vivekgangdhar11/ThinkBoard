0d275f00b44970f8e9f94bcb0ca063c1b00c2f6f
Vivek Gangdhar <vivekgangdhar11@gmail.com>
2025-10-30 21:20:53 +0530

    some changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -30,12 +30,13 @@ app.use("/api/notes", notesRoutes);
 
 // Serve frontend (React build) in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
   });
 }
+
 
 // Start server
 app.listen(PORT, () => {
